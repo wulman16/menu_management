5e6dadd4e90d98db5d0972da41db649ae70cedcf
Will Ulman <will.ulman@gmail.com>
2024-04-29 11:21:07 -0400

    Add update method to menu items

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,6 +1,6 @@
 class MenuItemsController < ApplicationController
   before_action :set_menu
-  before_action :set_menu_item, only: :show
+  before_action :set_menu_item, only: [:show, :update]
   
   def index
     render json: @menu.menu_items
@@ -14,6 +14,14 @@ class MenuItemsController < ApplicationController
     @menu_item = @menu.menu_items.new(menu_item_params)
     if @menu_item.save
       render json: @menu_item, status: :created, location: menu_menu_item_url(@menu, @menu_item)
+    else
+      render json: @menu_item.errors, status: :unprocessable_entity
+    end
+  end
+  
+  def update
+    if @menu_item.update(menu_item_params)
+      render json: @menu_item, status: :ok
     else
       render json: @menu_item.errors, status: :unprocessable_entity
     end
--- spec/requests/menu_items_spec.rb
+++ spec/requests/menu_items_spec.rb
@@ -73,4 +73,54 @@ RSpec.describe "MenuItems", type: :request do
       end
     end
   end
+  
+  describe 'PUT /menus/:menu_id/menu_items/:menu_item_id' do
+    let(:valid_attributes) { { menu_item: { name: 'Quinoa Burger', price: 11.99 } } }
+  
+    context 'when the menu item exists' do
+      before { put "/menus/#{menu_id}/menu_items/#{menu_item_id}", params: valid_attributes }
+  
+      it 'updates the record' do
+        updated_item = JSON.parse(response.body)
+        expect(updated_item['name']).to eq('Quinoa Burger')
+        expect(updated_item['price']).to eq('11.99')
+      end
+    
+      it 'returns the updated menu item' do
+        expect(response.body).to include('Quinoa Burger')
+        expect(response.body).to include('11.99')
+      end
+    
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+    
+    context 'when the menu item does not exist' do
+      let(:menu_item_id) { 0 }
+      before { put "/menus/#{menu_id}/menu_items/#{menu_item_id}", params: valid_attributes }
+      
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find MenuItem/)
+      end
+    end
+    
+    context 'with invalid attributes' do
+      let(:invalid_attributes) { { menu_item: { name: '', price: '' } } }
+      before { put "/menus/#{menu_id}/menu_items/#{menu_item_id}", params: invalid_attributes }
+  
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+  
+      it 'returns a validation failure message' do
+        expect(json['name']).to include("can't be blank")
+        expect(json['price']).to include("can't be blank")
+      end
+    end
+  end
 end
